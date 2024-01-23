5b2fd3a71656e515a95476253c91d0632264153f
santiago.gonzalez <santiago.gonzalezcourel@axpo.com>
2024-01-23 22:03:32 +0100

    first commit

---
+++ src/main/java/basic/virtual/threads/Launcher.java
@@ -0,0 +1,48 @@
+package basic.virtual.threads;
+
+import basic.virtual.threads.common.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Launcher
+{
+    public static void main(String[] args) {
+        try {
+            // Using a VirtualThreadPerTaskExecutor to execute tasks concurrently
+            try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+                List<Task> tasks = new ArrayList<>();
+
+                // Creating a list of 1000 tasks
+                for (int i = 0; i < 1000; i++) {
+                    tasks.add(new Task(i));
+                }
+
+                long startTime = System.currentTimeMillis();
+
+                // Invoking all tasks and obtaining a list of Futures
+                List<Future<Integer>> futures = executor.invokeAll(tasks);
+
+                long sum = 0;
+
+                // Summing up the results from each task
+                for (Future<Integer> future : futures) {
+                    sum += future.get();
+                }
+
+                long executionTime = System.currentTimeMillis() - startTime;
+
+                // Displaying the sum and total execution time
+                System.out.println("Sum = " + sum + "; Execution Time = " + executionTime + " ms");
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            // Handling exceptions
+            e.printStackTrace();
+        }
+    }
+
+}
